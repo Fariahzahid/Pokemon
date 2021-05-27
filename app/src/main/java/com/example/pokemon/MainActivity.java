d9a7e1cf085ef0cf63305b1bcea7b0137eb27226
fariah zahid <fariahzahid@gmail.com>
2021-05-27 16:05:59 +0100

    Splash Screen  Added
    Add splash screen at the start.

---
+++ app/src/main/java/com/example/pokemon/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.pokemon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.example.pokemon.Sign_In.Login;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    //Variables
+    Animation topAnimation, bottomAnimation;
+    ImageView splashscreen_pokemon_image,splashscreen_pokemon_logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Set Animation
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        splashscreen_pokemon_image = findViewById(R.id.splash_screen_pokemon_image);
+        splashscreen_pokemon_logo = findViewById(R.id.splash_screen_pokemon_logo);
+
+        splashscreen_pokemon_image.setAnimation(topAnimation);
+        splashscreen_pokemon_logo.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+
+}
---
+++ app/src/main/java/com/example/pokemon/Sign_In/Login.java
@@ -0,0 +1,17 @@
+package com.example.pokemon.Sign_In;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pokemon.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
+}
